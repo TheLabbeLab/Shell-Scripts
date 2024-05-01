67933c5ce95073722416c27b6beb5da355095597
PrayShah <prsh1291@colorado.edu>
2024-05-01 11:26:50 -0600

    Log File Have Dates!

--- run_all_gjfs_series.sh
+++ run_all_gjfs_series.sh
@@ -2,12 +2,12 @@
 #SBATCH --partition=amilan
 #SBATCH --qos=normal
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=8
+#SBATCH --ntasks-per-node=32
 #SBATCH --time=24:00:00
 #SBATCH --job-name=m062x
-#SBATCH --account=ucb273_peak1
+#SBATCH --account=ucb273_peak2
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=userid@colorado.edu
+#SBATCH --mail-user=prsh1291@colorado.edu
 
 module purge
 module load "gaussian/16_avx2"
@@ -16,7 +16,7 @@ FILES=*.gjf
 for f in $FILES
 do
  name=$(echo "$f" | cut -f 1 -d '.')
- g16 <$name.gjf> $name.log
+ g16 <$name.gjf> $name_${date + %Y-%m-%d}.log
   echo "Processing $name file..."
 done
 
