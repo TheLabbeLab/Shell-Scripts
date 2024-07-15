61fbb42a7a7543767caf4ec1eddaae0726f53a8e
PrayShah <prsh1291@colorado.edu>
2024-07-15 16:15:07 -0600

    Updated Gaussian!

--- Kinbot_Scripts/kinbot.sh
+++ Kinbot_Scripts/kinbot.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=4
 #SBATCH --time=24:00:00
 #SBATCH --job-name=Toluene_Radicals
-#SBATCH --account=ucb273_peak1
+#SBATCH --account=ucb273_peak2
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user= Enter Email
 
@@ -17,8 +17,11 @@ chmod 777 Transfer_Files.sh
 module purge
 module use /projects/nila3952/public/modules
 module load miniconda
-module load gaussian/16_avx2
+module load gaussian/g16_c.02
 source activate kinbot_2.2.1
+
+# Always specify a scratch directory on a fast storage space (not /home or /projects!)
+export GAUSS_SCRDIR="$(pwd)"
 
 kinbot kinbot.json
 
